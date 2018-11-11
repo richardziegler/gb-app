9abca037f68f651bde14d770c5674d16b4e2c338
Richard Ziegler <rzieglerdevelopment@gmail.com>
2018-11-11 02:02:46 -0600

    Initial Commit

---
+++ gb-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gb-app
+//
+//  Created by Richard Ziegler on 11/11/18.
+//  Copyright © 2018 Richard Ziegler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
